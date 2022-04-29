954553ab295ff40d8a6b9a9f01497f4c201f7ec9
carter <zongu3317@gmail.com>
2022-04-29 09:54:21 +0800

    [updated] 調整Producer寫法

--- KafkaPubSub.Domain/Applibs/KafkaEventProducer.cs
+++ KafkaPubSub.Domain/Applibs/KafkaEventProducer.cs
@@ -11,17 +11,23 @@ namespace KafkaPubSub.Domain.Applibs
     {
         private Lazy<IProducer<string, string>> producer;
 
-        private string brokerList;
-
-        private Acks acks;
-
         private ILogger logger;
 
         public KafkaEventProducer(string brokerList, Acks acks, ILogger logger)
         {
-            this.brokerList = brokerList;
-            this.acks = acks;
             this.logger = logger;
+
+            producer = new Lazy<IProducer<string, string>>(() =>
+            {
+                var config = new ProducerConfig()
+                {
+                    BootstrapServers = brokerList,
+                    MessageSendMaxRetries = 3,
+                    Acks = acks
+                };
+
+                return new ProducerBuilder<string, string>(config).Build();
+            });
         }
 
         public void Publish<T>(string topicName, int suffix, T data)
@@ -41,30 +47,6 @@ namespace KafkaPubSub.Domain.Applibs
                 {
                     this.logger.Trace($"KafkaEventProducer Partition: {deliveryResult.Result.Partition}, Offset: {deliveryResult.Result.Offset}");
                 });
-            }
-        }
-
-        public void Start()
-        {
-            producer = new Lazy<IProducer<string, string>>(() =>
-            {
-                var config = new ProducerConfig()
-                {
-                    BootstrapServers = this.brokerList,
-                    MessageSendMaxRetries = 3,
-                    Acks = this.acks
-                };
-
-                return new ProducerBuilder<string, string>(config).Build();
-            });
-        }
-
-        public void Stop()
-        {
-            if (producer.IsValueCreated)
-            {
-                producer.Value.Flush();
-                producer = null;
             }
         }
     }
